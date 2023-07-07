5c736a8c8110339ebbcfc90111a5768fc3a09006
Khochawongwat <86607408+Khochawongwat@users.noreply.github.com>
2023-07-07 21:26:29 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,71 +1,46 @@
 #include <iostream>
 #include "stack.h"
+#include <string>
 using namespace std;
+
+bool isMatchingPair(char open, char close) {
+    return (open == '(' && close == ')') ||
+           (open == '[' && close == ']') ||
+           (open == '{' && close == '}');
+}
+
+bool isBalanced(const string& expression, int& countOpen, int& countClose) {
+    Stack stack;
+    for (char c : expression) {
+        if (c == '(' || c == '[' || c == '{') {
+            stack.push(c);
+            countOpen++;
+        } else if (c == ')' || c == ']' || c == '}') {
+            if (stack.isEmpty() || !isMatchingPair(stack.get_top(), c)) {
+                countClose++;
+                continue;
+            }
+            stack.pop();
+            countOpen--;
+        }
+    }
+    
+    return stack.isEmpty() && countOpen == 0 && countClose == 0;
+}
 
 int main(int argc, char* argv[]) {
     if (argc > 1) {
         for (int i = 1; i < argc; ++i) {
             string expression = argv[i];
-            bool balanced = true;
-            bool stopped = false;
-            Stack stack;
             int countOpen = 0;
             int countClose = 0;
-          
-            for (char c : expression) {
-                switch (c) {
-                    case '[':
-                    case '{':
-                    case '(':
-                        stack.push(c);
-                        countOpen++;
-                        break;
-                    case ')': {
-                        if (stack.isEmpty() || stack.pop() != '(') {
-                            balanced = false;
-                            stopped = true;
-                        } else {
-                            countClose++;
-                        }
-                        break;
-                    }
-                    case ']': {
-                        if (stack.isEmpty() || stack.pop() != '[') {
-                            balanced = false;
-                            stopped = true;
-                        } else {
-                            countClose++;
-                        }
-                        break;
-                    }
-                    case '}': {
-                        if (stack.isEmpty() || stack.pop() != '{') {
-                            balanced = false;
-                            stopped = true;
-                        } else {
-                            countClose++;
-                        }
-                        break;
-                    }
-                    default:
-                        break;
-                }
-                
-                if (stopped) {
-                    countClose++;
-                    stopped = false;
-                }
-            }
-            
-            if (!stopped) {
-                balanced = balanced && stack.isEmpty();
-            }
+            bool balanced = isBalanced(expression, countOpen, countClose);
             
             if (balanced) {
                 cout << expression << ": Match" << endl;
             } else {
                 cout << expression << ": Not Match";
-                if (countClose < countOpen) {
+                if (countOpen > countClose) {
                     cout << " > Too many open parentheses";
                 } else if (countClose > countOpen) {
                     cout << " > Too many close parentheses";
